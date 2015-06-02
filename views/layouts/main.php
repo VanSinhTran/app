9e66f4b4c6d53ef731e5f283af16565e4ad081b4
Sinh TV <vansinh.tran@elinext.com>
2015-06-02 13:36:08 +0700

    Changed folder layout

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -49,6 +49,7 @@ AppAsset::register($this);
         ?>
 
         <div class="container">
+            <h1>This is Home Page</h1>
             <?= Breadcrumbs::widget([
                 'links' => isset($this->params['breadcrumbs']) ? $this->params['breadcrumbs'] : [],
             ]) ?>
